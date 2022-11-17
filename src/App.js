75f6fd98fea96d41d31c9c755d0ac583db2987fa
Dmitry Strizhakov <d.strizhakov@gmail.com>
2022-11-17 23:20:48 +0300

    add deadline date

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 
 import './App.less';
-import {useEffect, useState} from "react";
+import { useState } from "react";
 import {db} from "./firebase_config";
-
-import { doc, onSnapshot, serverTimestamp } from "firebase/firestore";
-import { collection, addDoc, query } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import TodoList from "./Components/TodoList";
+
 
 
 function App() {
     const [todoTitle, setTodoTitle] = useState("");
     const [todoDetails, setTodoDetails] = useState("");
+    const [todoDeadline, setTodoDeadline] = useState("");
 
+    const toTimestamp = (strDate) => {
+        var datum = Date.parse(strDate);
+        return {
+            seconds: datum/1000,
+            nanoseconds: 0
+        }
+    }
 
     const addTodo = async (event) => {
         event.preventDefault();
         if(todoTitle.length > 3){
             await addDoc(collection(db, "todos"), {
                 isdone: false,
-                timestamp: serverTimestamp(),
+                created: serverTimestamp(),
+                deadline: toTimestamp(todoDeadline),
                 todo: todoTitle,
                 details: todoDetails,
             })
@@ -30,8 +39,12 @@ function App() {
                 });
             setTodoDetails('');
             setTodoTitle('');
+            setTodoDeadline('')
         }
     }
+
+
+
 
   return (
     <div className="App">
@@ -42,6 +55,10 @@ function App() {
             <form className="App-form" action="submit">
                 <input type="text" value={todoTitle} onChange={e => setTodoTitle(e.target.value)} placeholder="Todo title"/>
                 <textarea name="Details" value={todoDetails} onChange={(e)=> setTodoDetails(e.target.value)} placeholder="Todo details" id=""  rows="5"></textarea>
+                <div className="form-data">
+                    <input type="file"/>
+                    <input type="date" value={todoDeadline} onChange={e => setTodoDeadline(e.target.value)}/>
+                </div>
                 <button type="submit" onClick={event =>addTodo(event)}>Create todo</button>
             </form>
             <section className="App-todolist">
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 const Todo = ({todo, deleteTodo, toggleComplete}) => {
+console.log (todo);
     return (
         <div className="todo">
             <div className="todo-main">
-                <h3 className="todo-title">{todo.todo}</h3>
+                <h2 className="todo-title">{todo.todo}</h2>
                 <p>{todo.details}</p>
+                <div className="todo-timestamp">
+                    {todo.created
+                        ?<p>Created: {new Date(todo.created.seconds*1000).toString()}</p>
+                        :<p>Created: no date</p>
+                    }
+                    {todo.deadline
+                        ?<p>Deadline: {new Date(todo.deadline.seconds*1000).toString()}</p>
+                        :<p>Deadline: no date</p>
+                    }
+                </div>
+
             </div>
             <div className="todo-progress">
                 <div >{todo.isdone
@@ -13,9 +25,9 @@ const Todo = ({todo, deleteTodo, toggleComplete}) => {
                 :<p className="red">In progress</p>
                 }
                 </div>
-                <button onClick={ () => toggleComplete(todo) }>Toggle</button>
+                <button onClick={() => toggleComplete(todo)}>Toggle</button>
             </div>
-            <button onClick={ () => deleteTodo(todo.id) }>x</button>
+            <button onClick={() => deleteTodo(todo.id)}>x</button>
         </div>
     );
 };
